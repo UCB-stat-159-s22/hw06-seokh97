b4d3e236e13850cd79e412c9749e7fe8133e4d76
Gyuhyeon Seo <seokh97@berkeley.edu>
2022-04-25 23:03:01 +0000

    created tests for utils.py / Edit later

---
+++ ligotools/tests/tests_utils.py
@@ -0,0 +1,15 @@
+import ligotools.readligo as rl
+import ligotools.utils as u 
+import numpy as np
+
+def test_whiten():
+	pass
+
+def test_write_wavfile():
+	pass
+
+def test_reqshift():
+	pass
+
+def test_SNR():
+	pass
